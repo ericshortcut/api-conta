b1a7d4b4e090e519333297b9d95ef7e4799c3b12
ericshortcut <ericshortcut@gmail.com>
2019-12-02 11:34:07 -0300

    Adding path context

--- src/main/java/com/cielo/desafio/conta/controller/ContaController.java
+++ src/main/java/com/cielo/desafio/conta/controller/ContaController.java
@@ -5,21 +5,20 @@ import com.cielo.desafio.conta.model.CustomPageable;
 import com.cielo.desafio.conta.service.ContaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("conta")
+@RequestMapping("contas")
 public class ContaController {
 
     @Autowired
     private ContaService contaService;
 
+    @CrossOrigin
     @GetMapping("/{numeroDaConta}")
-    public ResponseEntity<?> getConta(@PathVariable String numeroDaConta){
-        final CustomPageable<ControleLancamento> controleLancamento = contaService.getControleLancamento(numeroDaConta);
+    public ResponseEntity<?> getConta(@PathVariable() String numeroDaConta){
+        final CustomPageable<ControleLancamento>
+                controleLancamento = contaService.getControleLancamento(numeroDaConta);
         if( controleLancamento != null){
             return ResponseEntity.ok(controleLancamento);
         }else{
--- src/main/java/com/cielo/desafio/conta/service/ContaService.java
+++ src/main/java/com/cielo/desafio/conta/service/ContaService.java
@@ -2,7 +2,7 @@ package com.cielo.desafio.conta.service;
 
 import com.cielo.desafio.conta.model.ControleLancamento;
 import com.cielo.desafio.conta.model.CustomPageable;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,8 +10,8 @@ import org.springframework.web.client.RestTemplate;
 public class ContaService {
 
 
-    @Value("${application.url.controle-lancamento}")
-    private String urlLegado = "http://localhost:8080/mock-url-legado/";
+    // @Value("${application.url.controle-lancamento}")
+    private String urlLegado = "http://localhost:8080/v1/mock-url-legado/";
 
     public CustomPageable<ControleLancamento> getControleLancamento(String numeroDaConta){
         RestTemplate restTemplate = new RestTemplate();
